e9ed235e04bb0cfcf285f24ec7e31d12b1241051
Dakota Hanks <dakotahanks1020@gmail.com>
2022-05-04 06:41:26 -0400

    finished :>

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function openMenu() {
+    let menu = document.getElementById("menu"); 
+    menu.style.display = "block"; 
+    count += 1;
+    if (count == 2) {
+        menu.style.display = "none";
+        count = 0;
+    }
+}
